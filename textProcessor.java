035c798bd1ba7e53a5e5a84dfa175f72ba3faac6
Razan Al Raiisi <razanraiisi@gmail.com>
2025-07-23 20:13:03 +0400

    task 3: text processor

--- textProcessor.java
+++ textProcessor.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class textProcessor {
+    public static int countWords(String sentence) {
+
+        if (sentence == null || sentence.isEmpty()) {
+            return 0;
+        }
+
+
+        String[] words = sentence.split(" ");
+        return words.length;
+    }
+
+
+    public static String replaceWord(String text, String oldWord, String newWord) {
+        return text.replace(oldWord, newWord);
+    }
+
+    public static void main(String[] args) {
+        String sentence = "Java is fun and Java is powerful";
+        int wordCount = countWords(sentence);
+        System.out.println("Word count: " + wordCount);
+        String newSentence = replaceWord(sentence, "Java", "Programming");
+        System.out.println("Replaced sentence: " + newSentence);
+    }
 }
